1fcbd4720e8816c4da195592a5e7f5b6e0f568ae
faisal firaz <faisalfiraz@gmail.com>
2016-11-28 03:29:15 +0700

    hover-effect

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,3 @@
-$(document).ready(function() {
-
-	$("#nomi-demo").owlCarousel({
-
-                autoPlay: 2000, //Set AutoPlay to 3 seconds
-
-                items : 5,
-                itemsDesktop : [1199,3],
-                itemsDesktopSmall : [979,3]
-
-            });
-
-});
 
 // MapsJavascript
 
@@ -21,20 +8,4 @@ $(function() {
 		$('#div' + text).show();
                 // alert('Button ' + text + ' was clicked!');        
             });
-});
-
-
-$(document).ready(function() {
- 
-  $("#owl-demo").owlCarousel({
-    autoPlay : 3000,
-    stopOnHover : true,
-    navigation:true,
-    paginationSpeed : 1000,
-    goToFirstSpeed : 2000,
-    singleItem : true,
-    autoHeight : true,
-    transitionStyle:"fade"
-  });
- 
 });
